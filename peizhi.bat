f1bb8600ca2c9785dbfbdb5fd6e12a29e1d97746
toooney <toooney@126.com>
2018-10-15 16:33:12 +0800

    YourName

--- peizhi.bat
+++ peizhi.bat
@@ -1,3 +1,3 @@
-git@github.com:ncst-robot/bigger.git
+runas /user:administrator cmd
 pip install sphinx recommonmark sphinx-autobuild sphinx_rtd_theme -i http://mirrors.aliyun.com/pypi/simple/
 sphinx-quickstart
