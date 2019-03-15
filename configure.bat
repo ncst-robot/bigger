41fed59e56b3ff63329e6c5bd14751dd3754f00a
toooney <toooney@126.com>
2019-03-15 16:50:51 +0800

    update all

--- configure.bat
+++
@@ -1,4 +0,0 @@
-git config --global user.email "toooney@126.com"
-git config --global user.name "toooney"
-pip install sphinx recommonmark sphinx-autobuild sphinx_rtd_theme -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host=mirrors.aliyun.com
-python -m pip install --upgrade pip -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host=mirrors.aliyun.com
--- submit.bat
+++
@@ -1,4 +0,0 @@
-git add .	
-git commit -m "toooney"
-git pull	
-git push
